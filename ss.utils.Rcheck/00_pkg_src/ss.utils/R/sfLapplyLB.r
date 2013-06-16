9276419de952e8c8c210ab633982a0bf6ec04adf
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2013-06-16 18:40:19 +0900

    removed unncessary files in repository

--- ss.utils.Rcheck/00_pkg_src/ss.utils/R/sfLapplyLB.r
+++
@@ -1,9 +0,0 @@
-## Lord balancing version of sfLapply to use parLapplyLB
-
-sfLapplyLB<-function (x, fun, ...){
-    sfCheck()
-    checkFunction(fun)
-    if (sfParallel())
-        return(parLapplyLB(sfGetCluster(), x, fun, ...))
-    else return(lapply(x, fun, ...))
-}
--- ss.utils.Rcheck/00_pkg_src/ss.utils/examples/do.boot.hpc.r
+++
@@ -1,3 +0,0 @@
-source("/home/Rscripts/ss_bootstrap.r")
-res<-parSsboot(mcore=11,nboot=300,,namesfile="Starter.SS",orgdir="./orgrun",
-  control.boot="control_boot_scale10.ss"ss3.arg=" -nox -nohess")
